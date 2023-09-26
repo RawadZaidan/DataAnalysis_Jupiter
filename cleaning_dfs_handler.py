e6dcfc8532d9a203a922719d087cc3bea80dd19c
RawadZaidan <rawad.o.zaidan@gmail.com>
2023-09-26 18:42:07 +0300

    Fixed cleaning_handler

--- cleaning_dfs_handler.py
+++ cleaning_dfs_handler.py
@@ -1,6 +1,6 @@
 from lookups import ErrorHandling
 from logging_handler import show_error_message
-from pandas_handler import process_net_transfer_record,drop_nulls,fill_nulls
+from pandas_handler import process_net_transfer_record, drop_nulls, fill_nulls
 import pandas as pd
 
 #Function to clean clubs df
@@ -15,6 +15,25 @@ def clean_clubs_function(df):
     
     except Exception as e:
         error_string_prefix = ErrorHandling.CLUBS_ERROR.value
+        error_string_suffix = str(e)
+        show_error_message(error_string_prefix, error_string_suffix)
+
+#Function to clean player valuations df
+def clean_player_valuations_function(df):
+    try:
+        columns_to_show = ['player_id','last_season','date','market_value_in_eur','current_club_id','player_club_domestic_competition_id']
+        df = df[columns_to_show]
+        df['date'] = pd.to_datetime(df['date'])
+        df = df.loc[(df['player_club_domestic_competition_id'] == 'GB1') & 
+                    (df['date'] >= pd.to_datetime('2018-08-10')) & 
+                    (df['last_season'] >= 2018) &
+                    ((df['last_season'] == df['date'].dt.year) | (df['last_season'] == (df['date'].dt.year - 1)))]
+        df = df.sort_values(by='date', ascending=True)
+        df.rename(columns={'player_club_domestic_competition_id': 'competition_id'}, inplace=True)
+        df.reset_index(inplace=True, drop=True)
+        return df
+    except Exception as e:
+        error_string_prefix = ErrorHandling.PLAYERVALUATIONS_ERROR.value
         error_string_suffix = str(e)
         show_error_message(error_string_prefix, error_string_suffix)
 
