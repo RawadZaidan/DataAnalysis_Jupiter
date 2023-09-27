ce19bf01ba4726c706f02bdd1a83548a9b06fc94
RawadZaidan <rawad.o.zaidan@gmail.com>
2023-09-27 20:48:50 +0300

    no message

--- V_Rawad.sql
+++ V_Rawad.sql
@@ -1,3 +1,4 @@
+--seasons dim table 
 CREATE TABLE IF NOT EXISTS premier_league.dim_season (
     season_id SERIAL PRIMARY KEY,
     start_date DATE,
@@ -6,24 +7,16 @@ CREATE TABLE IF NOT EXISTS premier_league.dim_season (
 );
 CREATE INDEX IF NOT EXISTS idx_season_id ON premier_league.dim_season(season_id);
 
-INSERT INTO premier_league.dim_season
-   (season_id, start_date, end_date, league_name)
-SELECT
---    scr_games.game_id,
---    scr_games.date,
---    scr_games.home_club_id,
---    scr_games.away_club_id,
---    scr_games.stadium,
---    scr_games.referee 
-FROM premier_league.stg_games as scr_games
-ON CONFLICT (match_id)
-DO UPDATE SET 
-   match_id = excluded.match_id,
-   match_date = excluded.match_date,
-   home_team_id = excluded.home_team_id,
-   away_team_id = excluded.away_team_id,
-   stadium_name = excluded.stadium_name,
-   referee_name = excluded.referee_name;
+-- Insert seasons from 2018 to 2023 into the dim_season table
+INSERT INTO premier_league.dim_season (season_id, start_date, end_date, league_name)
+VALUES
+    (2018,'2018-08-01', '2019-05-31', 'Premier League'),
+    (2019,'2019-08-01', '2020-05-31', 'Premier League'),
+    (2020,'2020-08-01', '2021-05-31', 'Premier League'),
+    (2021,'2021-08-01', '2022-05-31', 'Premier League'),
+    (2022,'2022-08-01', '2023-05-31', 'Premier League'),
+    (2023,'2022-08-01', '2023-05-31', 'Premier League');
+
 ----------------------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS premier_league.fact_player_performance (
     match_id INT REFERENCES premier_league.dim_match(match_id),
