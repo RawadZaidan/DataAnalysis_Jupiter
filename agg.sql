70a55940ee57b5cfd8d072824a6edc0a25f5848a
RawadZaidan <rawad.o.zaidan@gmail.com>
2023-09-30 19:17:57 +0300

    Added season, winner

--- agg.sql
+++ agg.sql
@@ -53,3 +53,31 @@ SELECT result,
 FROM history
 GROUP BY result
 
+
+WITH wind  AS (
+SELECT CASE
+        WHEN home_team_goals > away_team_goals
+		THEN home_team_name
+		WHEN home_team_goals < away_team_goals
+		THEN away_team_name
+        ELSE 'Draw'
+    END AS winner,
+*  FROM premier_league.fact_game_results
+ORDER BY match_id
+)
+
+SELECT CASE WHEN 
+			SUBSTRING(CAST(match_id AS TEXT), 0, 3) = '38' THEN 2018
+			WHEN 
+			SUBSTRING(CAST(match_id AS TEXT), 0, 3) = '46' THEN 2019
+			WHEN 
+			SUBSTRING(CAST(match_id AS TEXT), 1, 2) IN ('58', '59') THEN 2020
+			WHEN 
+			SUBSTRING(CAST(match_id AS TEXT), 0, 3) = '66' THEN 2021
+			WHEN 
+			SUBSTRING(CAST(match_id AS TEXT), 0, 3) IN ('74','75') THEN 2022
+			WHEN 
+			SUBSTRING(CAST(match_id AS TEXT), 0, 3) = '93' THEN 2023
+			END AS season,*
+FROM wind 
+
